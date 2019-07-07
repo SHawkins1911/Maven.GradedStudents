c538dbe96ab7f36a70663f055aad31c6ba87e2e8
Stefun <hawkd215@gmail.com>
2019-07-07 00:37:56 -0400

    1 method unfinished

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,4 +1,47 @@
 package io.zipcoder;
 
 public class Classroom {
-}
+  private Student [] students;
+  private int maxNumberOfStudents;
+  private Student [] preEnrollment;
+  private Student [] postEnrollment;
+
+    public void setPreEnrollment(Student[] preEnrollment) {
+        this.preEnrollment = students;
+    }
+
+    public void setPostEnrollment(Student[] postEnrollment) {
+        this.postEnrollment = new Student[preEnrollment.length+1];
+    }
+
+    public Classroom(int maxNumberOfStudents){
+        this.maxNumberOfStudents = maxNumberOfStudents;
+    }
+    public Classroom(Student[] students){
+        this.students = students;
+    }
+    public Classroom(){
+        this.students = new Student[30];
+    }
+
+    public Student[] getStudents() {
+        return students;
+    }
+
+    public Student [] addStudent(Student student){
+        postEnrollment[postEnrollment.length-1] = student;
+       return postEnrollment;
+    }
+    public Double getClassAverageExamScore(){
+        Double result;
+        Double examTotals = 0.0;
+        for (Student student : students) {
+
+            examTotals += student.getAveragExamScore();
+        }
+        result = examTotals/students.length;
+        System.out.println(result);
+        return result;
+    }
+    }
+
--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,65 @@
 package io.zipcoder;
 
+
+import java.util.ArrayList;
+
 public class Student {
+ private String firstName;
+ private String lastName;
+ private ArrayList<Double> examScores = new ArrayList<>();
+ private Integer numberOfExamsTaken = 0;
+
+    public Student(String firstName, String lastName, ArrayList<Double> examScores){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.examScores = examScores;
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setExamScores(ArrayList<Double> examScores) {
+        this.examScores = examScores;
+    }
+
+    public void setExamScore(int examNumber, Double newExamScore ){
+        examScores.add(examNumber, newExamScore);
+
+    }
+
+    public Integer getNumberOfExamsTaken(){
+        return numberOfExamsTaken;
+    }
+
+    public void getExamScores(){
+        System.out.println(examScores.toString());
+    }
+
+    public void addExamScore(Double examScore){
+        examScores.add(examScore);
+    }
+
+    public Double getAveragExamScore(){
+        Double result;
+        Double examTotals = 0.0;
+        for (Double examScore : examScores) {
+            examTotals += examScore;
+        }
+        result = examTotals/examScores.size();
+        return result;
+    }
 }
